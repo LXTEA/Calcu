4e596f9a73d6a2f3374caacab58c03f71cc96cbc
LXTEA <1out1@mail.ru>
2023-08-05 20:35:33 +0700

    cackulyator

--- src/Main.java
+++ src/Main.java
@@ -4,60 +4,37 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException{
-
         Scanner reader = new Scanner(System.in);
-        System.out.print("Введите пример:");
-        int cifra1 = reader.nextInt();
-        if(cifra1 > 10) {
-                throw new IOException("Первое число больше 10");
+        String calc = reader.nextLine();
+        System.out.println(calc(calc));
+    }
+    public static String calc(String input) throws IOException{
+        int sum = 0;
+        String[] probel = input.split(" ");
+        int a, b;
+        a = Integer.parseInt(probel[0]);
+        b = Integer.parseInt(probel[2]);
+        String znak = probel[1];
+        if (probel.length != 3){
+            throw new IOException();
         }
-        if(cifra1 == 0) {
-            throw new IOException("Первое число меньше 1");
+        if (a > 10 || a < 1){
+            throw new IOException();
         }
-        char znak = reader.next().charAt(0);
-
-        int cifra2 = reader.nextInt();
-        if(cifra2 > 10) {
-            throw new IOException("Второе число больше 10");
+        if (b > 10 || b < 1){
+            throw new IOException();
         }
-        if(cifra2 == 0) {
-            throw new IOException("Второе число меньше 1");
-        }
-        int o = 0;
-
         switch (znak) {
-            case '+' : o = cifra1 + cifra2;
+            case "+" : sum = a + b;
                 break;
-            case '-' : o = cifra1 - cifra2;
+            case "-" : sum = a - b;
                 break;
-            case '*' : o = cifra1 * cifra2;
+            case "*" : sum = a * b;
                 break;
-            case '/' : o = cifra1 / cifra2;
+            case "/" : sum = a / b;
                 break;
-            default:  System.out.printf("Ошибка! Введите корректный знак");
-                return;
+            default:  System.out.println("Ошибка! Введите корректный знак");
         }
-        System.out.print("\nОтвет:\n");
-        System.out.printf(cifra1 + " " + znak + " " + cifra2 + " = " + o);
-
-
-
+        return String.valueOf(sum);
     }
 }
-//Scanner space = new Scanner(System.in);
-//        String calc = space.nextLine();
-//        int sum = 0;
-//        String[] probel = calc.split(" ");
-//        int a, b;
-//        a = Integer.parseInt(probel[0]);
-//        b = Integer.parseInt(probel[2]);
-//        if (probel.length == 3){
-//            sum = a + b;
-//            System.out.print("= " +sum);
-//        }
-//        else {
-//            throw new IOException();
-//        }
-//        if (probel.length < 3){
-//            throw new IOException();
-//        }
